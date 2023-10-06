13e942aebde3064bb2ad18edf19edd77ad0cccee
ivnnis <ivnnis@gmail.com>
2023-10-06 12:31:27 +0300

    добавил исключения

--- bot.py
+++ bot.py
@@ -9,30 +9,33 @@ bot = telebot.TeleBot(TOKEN)
 bot.remove_webhook()
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет! скинь ник пользователя в формате @abc")
+    try:
+        bot.send_message(message.chat.id, "Привет! Перешли сообщение в этот чат для записи")
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения: {e}")
 
-<<<<<<< HEAD
+# Обработчик для пересылаемых сообщений
 @bot.message_handler(func=lambda message: message.forward_from is not None)
-=======
-@bot.message_handler(func=lambda message: True)
->>>>>>> 89ce18739b40ecaa5ac481b6ee683134764aafc5
 def handle_forwarded_message(message):
-    # Получаем ник автора пересылаемого сообщения
-    author_username = message.forward_from.username
-    
-    if author_username:
-        with open(filename, 'a') as file:
-            # Проверяем, есть ли упоминание уже в файле
-            if author_username not in open(filename, 'r').read():
-                # Записываем новое упоминание в файл
-                file.write(author_username + '\n')
-            else:
-<<<<<<< HEAD
-                bot.send_message(message.chat.id, f"Предупреждение: {author_username} уже был.")
-=======
-                bot.send_message(message.chat.id, f"Предупреждение: {author_username} уже был")
->>>>>>> 89ce18739b40ecaa5ac481b6ee683134764aafc5
-                # Если упоминание уже есть, не записываем его в файл
+    try:
+        # Получаем ник автора пересылаемого сообщения
+        author_username = message.forward_from.username
+        
+        if author_username:
+            with open(filename, 'a') as file:
+                # Проверяем, есть ли упоминание уже в файле
+                if author_username not in open(filename, 'r').read():
+                    # Записываем новое упоминание в файл
+                    file.write(author_username + '\n')
+                else:
+                    bot.send_message(message.chat.id, f"Предупреждение: {author_username} уже был упомянут в этом чате.")
+                    # Если упоминание уже есть, не записываем его в файл
+    except Exception as e:
+        print(f"Ошибка при обработке пересылаемого сообщения: {e}")
 
 # Запускаем бота
-bot.polling()
+while True:
+    try:
+        bot.polling()
+    except Exception as e:
+        print(f"Ошибка при запуске бота: {e}")
